2c529b4dd2f7c2e3f1c53978beb9fa79a2d6e272
punit kumar <139279217+punitSheoran24@users.noreply.github.com>
2026-02-04 19:03:45 +0530

    Time: 50 ms (31.99%) | Memory: 25.1 MB (11.47%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -2,15 +2,11 @@ class RecentCounter:
 
     def __init__(self):
         self.queue = []
+        self.start = 0
 
     def ping(self, t: int) -> int:
-        range = t - 3000
-        if range <= 0:
-            self.queue.append(t)
-        elif self.queue and range <= self.queue[0]:
-            self.queue.append(t)
-        else:
-            while self.queue and range > self.queue[0]:
-                self.queue.pop(0)
-            self.queue.append(t)
-        return len(self.queue)
+        self.queue.append(t)
+        while (t - 3000) > self.queue[self.start]:
+            self.start += 1
+
+        return len(self.queue) - self.start
