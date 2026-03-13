7b242c16a470decb3c826249064d13a0d6d49f40
punit kumar <139279217+punitSheoran24@users.noreply.github.com>
2026-03-14 05:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.83%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -3,10 +3,5 @@ class Solution:
         min_cost=[cost[0],cost[1]]
         print(min_cost)
         for i in range(2,len(cost)):
-            if cost[i]+min_cost[i-1]>cost[i]+min_cost[i-2]:
-                min_cost.append(cost[i]+min_cost[i-2])
-            else:
-                min_cost.append(cost[i]+min_cost[i-1])
-
-        
-        return min_cost[-1] if min_cost[-1]<min_cost[-2] else min_cost[-2]
+            min_cost.append(min(min_cost[i-1],min_cost[i-2])+cost[i])
+        return min(min_cost[-1],min_cost[-2])
