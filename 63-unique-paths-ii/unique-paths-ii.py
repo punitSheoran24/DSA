974f6da3a50d624430a8ea17fee74c70ae9622c7
punit kumar <139279217+punitSheoran24@users.noreply.github.com>
2026-03-15 19:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (97.10%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])
-        if obstacleGrid[0][0] == 1 or obstacleGrid[row - 1][col - 1] == 1:
-            return 0
         lst = [0] * col
         lst[0] = 1
         for i in range(row):
@@ -13,4 +13,3 @@ class Solution:
                 elif j > 0:
                     lst[j] = lst[j] + lst[j - 1]
         return lst[-1]
-
