36afa0bfe48ac488fa914c468f3d4f7d833a48b6
punit kumar <139279217+punitSheoran24@users.noreply.github.com>
2026-03-18 00:18:11 +0530

    Time: 13 ms (28.80%) | Memory: 20.3 MB (38.76%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,14 +1,30 @@
+# class Solution:
+#     def countBits(self, n: int) -> list[int]:
+#         def helper(n):
+#             if n == 0:
+#                 return 0
+#             if n == 1:
+#                 return 1
+#             binary = format(n, 'b')
+#             return 1 + helper(int(binary[1:], 2))
+
+#         res = []
+#         for i in range(n + 1):
+#             res.append(helper(i))
+#         return res
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        def helper(n):
-            if n == 0:
-                return 0
-            if n == 1:
-                return 1
-            binary = format(n, 'b')
-            return 1 + helper(int(binary[1:], 2))
-
-        res = []
-        for i in range(n + 1):
-            res.append(helper(i))
+        if n == 0:
+            return [0]
+        if n == 1:
+            return [0, 1]
+        res = [0, 1]
+        prev, cur = 0, 2
+        for i in range(2, n + 1):
+            binary = format(i, 'b')
+            if cur != len(binary):
+                cur = len(binary)
+                prev = 0
+            res.append(1 + res[prev])
+            prev += 1
         return res
