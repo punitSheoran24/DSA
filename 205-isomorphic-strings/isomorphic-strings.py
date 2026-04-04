98980f8f5542654b17251ab4250a3c5270ccd6e8
punit kumar <139279217+punitSheoran24@users.noreply.github.com>
2026-04-04 12:50:20 +0530

    Time: 7 ms (38.63%) | Memory: 19.3 MB (63.01%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,10 +1,22 @@
+# class Solution:
+#     def isIsomorphic(self, s: str, t: str) -> bool:
+#         map = {}
+#         for i in range(len(s)):
+#             if s[i] not in map and t[i] not in map.values():
+#                 map[s[i]] = t[i]
+#             elif not map.get(s[i], 0) == t[i]:
+#                 return False
+
+#         return True
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        map = {}
+        smap = {}
+        tmap = {}
         for i in range(len(s)):
-            if s[i] not in map and t[i] not in map.values():
-                map[s[i]] = t[i]
-            elif not map.get(s[i], 0) == t[i]:
+            if s[i] not in smap:
+                smap[s[i]] = i
+            if t[i] not in tmap:
+                tmap[t[i]] = i
+            if smap[s[i]] != tmap[t[i]]:
                 return False
-
         return True
